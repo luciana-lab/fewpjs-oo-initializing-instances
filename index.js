0db9bccd5b1346e26c0af8799b2b0ccb25a4bb4b
Luciana Sugeta <lulusugeta@gmail.com>
2021-09-17 10:15:45 -0400

    commit tree a762429d86741d9bb7e9a9bba356729476752259

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here
+// breakfast
+// 1) has a class of breakfast
+// 2) creates a new breakfast with the correct parameters
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    };
+};
+
+// lunch
+// 1) has a class of lunch
+// 2) creates a new lunch with the correct parameters
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    };
+};
+
+// dinner
+// 1) has a class of dinner
+// 2) creates a new dinner with the correct parameters
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+    };
+};
